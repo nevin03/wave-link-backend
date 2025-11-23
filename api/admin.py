3166604ba3ee9bf4d00ce0ea3eaa5b70bcc778b2
nevin03 <nevinm2021@gmail.com>
2025-11-23 12:54:07 +0530

    fix : history entries

--- api/admin.py
+++ api/admin.py
@@ -3,10 +3,22 @@ from .models import TermsAndConditions, PrivacyPolicy
 
 @admin.register(TermsAndConditions)
 class TermsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'updated_at']
+    list_display = ['__str__', 'updated_at']
     ordering = ['-updated_at']
+    
+    def has_add_permission(self, request):
+        return not TermsAndConditions.objects.exists()
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 @admin.register(PrivacyPolicy)
 class PrivacyAdmin(admin.ModelAdmin):
-    list_display = ['id', 'updated_at']
+    list_display = ['__str__', 'updated_at']
     ordering = ['-updated_at']
+    
+    def has_add_permission(self, request):
+        return not PrivacyPolicy.objects.exists()
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
--- api/models.py
+++ api/models.py
@@ -4,12 +4,20 @@ class TermsAndConditions(models.Model):
     content = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
 
+    def save(self, *args, **kwargs):
+        self.__class__.objects.all().delete()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return "Terms & Conditions"
 
 class PrivacyPolicy(models.Model):
     content = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.__class__.objects.all().delete()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return "Privacy Policy"
